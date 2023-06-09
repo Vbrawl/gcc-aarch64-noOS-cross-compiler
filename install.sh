49cc1aedab2d297943ac653ec0371185dd8fe646
Vbrawl <greninjagr@gmail.com>
2023-06-09 17:20:53 +0300

    Added dependencies auto-installation.

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/bash
 
+# Install dependencies (https://wiki.osdev.org/GCC_Cross-Compiler#Preparing_for_the_build)
+sudo apt update
+sudo apt install build-essential -y
+sudo apt install bison -y
+sudo apt install flex -y
+sudo apt install libgmp3-dev -y
+sudo apt install libmpc-dev -y
+sudo apt install libmpfr-dev -y
+sudo apt install texinfo -y
+sudo apt install libcloog-isl-dev -y
+sudo apt install libisl-dev -y
+
+
+
+
 export BIN_URL="https://ftp.gnu.org/gnu/binutils/binutils-2.40.tar.gz"
 export GCC_URL="https://ftp.gnu.org/gnu/gcc/gcc-13.1.0/gcc-13.1.0.tar.gz"
 
